bf8ff163ba495f93ded94e1cb55966823b4e6430
Mike Jackson <mike.jackson@mjconsult.co.uk>
2023-09-04 15:19:12 +0100

    activity01 complete

---
+++ activity01.js
@@ -0,0 +1,11 @@
+let name = "Mike";
+let age = 30 // I wish;
+let favouriteColour = "red";
+
+console.log(`My name is ${name}. I am ${age} and my favourite colour is ${favouriteColour}.`);
+
+name = "Paul";
+age = 35;
+favouriteColour = "blue";
+
+console.log(`My name is ${name}. I am ${age} and my favourite colour is ${favouriteColour}.`);
