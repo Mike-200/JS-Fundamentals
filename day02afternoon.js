d1c03b6a1d6665496857ef1b420f8dfecb72a630
Mike Jackson <mike.jackson@mjconsult.co.uk>
2023-09-05 16:09:09 +0100

    arrays activity 1 complete

---
+++ day02afternoon.js
@@ -0,0 +1,19 @@
+// Arrays
+
+// Activity 1
+
+const favouriteSongs = [
+  "P!nk - Trustfall",
+  "P!nk - Never Gonna Not Dance Again",
+  "Disturbed - The Sound of Silence",
+];
+console.log(favouriteSongs);
+
+// Adding 2 more items to the array
+favouriteSongs.push("Mike Oldfield - Moonlight Shadow");
+favouriteSongs.push("Tensnake - See Right Through");
+console.log(favouriteSongs);
+
+// Removing the last item off the array
+favouriteSongs.pop();
+console.log(favouriteSongs);
