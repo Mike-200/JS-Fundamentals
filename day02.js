076c7a694558b657558b328b1dc35acfa2177444
Mike Jackson <mike.jackson@mjconsult.co.uk>
2023-09-05 11:46:41 +0100

    activity 2 complete

--- day02.js
+++ day02.js
@@ -8,3 +8,23 @@ if (age > 17 && country == "UK") {
 } else {
   console.log("You aren't old enough");
 }
+
+// Activity 2
+
+const topping = "celery";
+switch (topping) {
+  // These are my favourite ingredients:-
+  case "ham":
+  case "cheese":
+  case "chicken":
+  case "pork":
+    console.log("These are important ingredients for my pizza.");
+    break;
+  // I don't mind these ingredients
+  case "tomatoes":
+  case "onions":
+    console.log(`I don’t mind having ${topping} on my pizza`);
+    break;
+  default:
+    console.log(`${topping} should not be on a pizza.`);
+}
